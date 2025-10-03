8b5ce8dd99ed1191a46f7f286deea0d32ab0c805
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-10-03 08:53:32 -0400

    Create Ex01.java

---
+++ src/ExerciciosCursoJava/Exercicios01/Ex01.java
@@ -0,0 +1,24 @@
+package ExerciciosCursoJava.Exercicios01;
+
+import java.util.Scanner;
+
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa */
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int A, B, soma;
+
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		soma = A + B;
+
+		System.out.println("SOMA = " + soma);
+
+		sc.close();
+	}
+}
