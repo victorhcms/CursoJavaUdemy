52a82b6cb9a4b0ae5f295b8eaf92fc1931bd1b21
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-10-03 08:53:35 -0400

    Create Ex02.java

---
+++ src/ExerciciosCursoJava/Exercicios01/Ex02.java
@@ -0,0 +1,35 @@
+package ExerciciosCursoJava.Exercicios01;
+
+import java.util.Locale;
+import java.util.Scanner;
+/* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2
+Considere o valor de π = 3.14159
+ */
+
+public class Ex02 {
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio;
+
+        double pi;
+
+        double area;
+
+        pi = 3.14159;
+
+        raio = sc.nextDouble();
+
+        area = pi * raio * raio;
+
+    
+        System.out.printf("A= %.4f%n", area);
+
+        sc.close();
+
+    }
+}
