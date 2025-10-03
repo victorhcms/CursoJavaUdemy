8ad767a077680e50fe55858cf4258484782cfdbd
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-10-03 08:53:37 -0400

    Create Ex03.java

---
+++ src/ExerciciosCursoJava/Exercicios01/Ex03.java
@@ -0,0 +1,29 @@
+package ExerciciosCursoJava.Exercicios01;
+
+/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). */
+
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, C, D, dif;
+        
+        A = sc.nextInt();
+
+        B = sc.nextInt();
+
+        C = sc.nextInt();
+        
+        D = sc.nextInt();
+
+        dif = A * B - C * D;
+
+        System.out.println("DIFERENCA = " + dif);
+
+        sc.close();
+    }
+}
