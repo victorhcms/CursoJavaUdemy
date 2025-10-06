5686174cfccfe2eafa9bb93fc2ab5baffddfedaf
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-10-05 21:51:39 -0400

    Update Ex01.java

--- src/ExerciciosCursoJava/Exercicios01/Ex01.java
+++ src/ExerciciosCursoJava/Exercicios01/Ex01.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
 mensagem explicativa */
 public class Ex01 {
-
+        @SuppressWarnings("ConvertToTryWithResources")
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
