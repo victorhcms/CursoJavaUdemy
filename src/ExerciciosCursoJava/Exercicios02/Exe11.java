6fa2c7166041d0b47a6ac55dbabc89f9234dbe0e
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-10-09 08:13:25 -0400

    Att.

---
+++ src/ExerciciosCursoJava/Exercicios02/Exe11.java
@@ -0,0 +1,13 @@
+package ExerciciosCursoJava.Exercicios02;
+
+import java.util.Scanner;
+
+public class Exe11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
