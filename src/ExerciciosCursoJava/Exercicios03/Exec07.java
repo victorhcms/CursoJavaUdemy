a6888fb2cb80a8537534b11b8bb162761e00741f
victorhcms <mancaneiravictordev@gmail.com>
2025-10-27 21:02:32 -0400

    ./

---
+++ src/ExerciciosCursoJava/Exercicios03/Exec07.java
@@ -0,0 +1,11 @@
+package ExerciciosCursoJava.Exercicios03;
+
+public class Exec07 {
+    public static void main(String[] args) {
+        
+        for (int i=10; i>0; i--) {
+            System.out.println("Valor de i: " + i);
+        }
+
+    }
+}
