2d0134e23877bf8434ad531522ea788edc0573bb
victorhcms <mancaneiravictordev@gmail.com>
2025-10-27 20:55:45 -0400

    ..

--- src/ExerciciosCursoJava/Exercicios03/Exec06.java
+++ src/ExerciciosCursoJava/Exercicios03/Exec06.java
@@ -2,21 +2,20 @@ package ExerciciosCursoJava.Exercicios03;
 
 import java.util.Scanner;
 
-public class Exec05 {
+public class Exec06 {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
+        int N = sc.nextInt();
 
         int soma = 0;
-        while (x != 0) {
+        for (int i=0; i<N; i++) {
+            int x = sc.nextInt();
             soma = soma + x;
-            x = sc.nextInt();
         }
 
         System.out.println(soma);
-        
+
         sc.close();
     }
 }
