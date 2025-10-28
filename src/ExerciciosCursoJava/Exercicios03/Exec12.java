aa4a2c43c0113d00e74ede8afaf7d1246e8fd918
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-10-28 08:47:09 -0400

    ..

---
+++ src/ExerciciosCursoJava/Exercicios03/Exec12.java
@@ -0,0 +1,14 @@
+package ExerciciosCursoJava.Exercicios03;
+
+public class Exec12 {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        do {
+            System.out.println("Contagem: " + contador);
+            contador++;
+        } while (contador <= 5);
+
+        System.out.println("Fim do loop!");
+    }
+}
