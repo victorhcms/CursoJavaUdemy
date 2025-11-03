58e46d74e056d30a216d7a30ee1f8e99d685e9f0
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-11-03 10:30:22 -0400

    ..

--- src/ExerciciosCursoJava/Exercicios04/Exerc01.java
+++ src/ExerciciosCursoJava/Exercicios04/Exerc01.java
@@ -1,7 +1,17 @@
 package ExerciciosCursoJava.Exercicios04;
 
+import java.util.Scanner;
+
 public class Exerc01 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("INICIANDO A CONTAGEM");
         
+        for (int i=100; i>= 0; i--) {
+            System.out.println("Contagem regresiva: " + i);
+        }
+
+        sc.close();
     }
 }
---
+++ src/ExerciciosCursoJava/Exercicios04/Exerc02.java
@@ -0,0 +1,7 @@
+package ExerciciosCursoJava.Exercicios04;
+
+public class Exerc02 {
+    public static void main(String[] args) {
+        
+    }
+}
