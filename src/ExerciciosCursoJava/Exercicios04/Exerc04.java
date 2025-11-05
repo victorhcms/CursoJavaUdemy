3cd893555f15e814dc8e69b729fd81f1f8c58bf7
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-11-05 09:28:31 -0400

    .

---
+++ src/ExerciciosCursoJava/Exercicios04/Exerc04.java
@@ -0,0 +1,15 @@
+package ExerciciosCursoJava.Exercicios04;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc04 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
