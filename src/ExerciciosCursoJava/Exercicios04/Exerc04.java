ef7d9c26078f370e4e1fad7cc9e03a61b1d686b2
Victor Mançaneira <mancaneiravictordev@gmail.com>
2025-11-05 09:59:52 -0400

    .

--- src/ExerciciosCursoJava/Exercicios04/Exerc04.java
+++ src/ExerciciosCursoJava/Exercicios04/Exerc04.java
@@ -7,9 +7,9 @@ public class Exerc04 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
 
-        
-
+        System.out.println("O valor digitado foi: " + X);
         sc.close();
     }
 }
